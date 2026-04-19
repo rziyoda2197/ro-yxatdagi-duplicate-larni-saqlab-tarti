91aa97e41332751e464037b78a202f94a665af8c
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-19 19:51:02 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,234 @@
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2, 3, 2, 4, 5, 5, 6, 7, 8, 8, 9]
+print(qaytarish(ro'yxat))
+```
+
+```python
+def qaytarish(ro'yxat):
+    qaytarilgan = []
+    for i in ro'yxat:
+        if i not in qaytarilgan:
+            qaytarilgan.append(i)
+        else:
+            qaytarilgan.append(i)
+    return qaytarilgan
+
+ro'yxat = [1, 2
